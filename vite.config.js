aa4f5b3c71bfb74ea1316e2a6f0363c8d4a8703e
Mohamed <010232066mo@gmail.com>
2025-10-06 19:39:38 +0300

    'commit'

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,22 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   base: "/Space-tourism-website/",
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: "/index.html",
+        "destination-europa": "/destination-europa.html",
+        "destination-mars": "/destination-mars.html",
+        "destination-moon": "/destination-moon.html",
+        "destination-titan": "/destination-titan.html",
+        "crew-commander": "/crew-commander.html",
+        "crew-engineer": "/crew-engineer.html",
+        "crew-pilot": "/crew-pilot.html",
+        "crew-specialist": "/crew-specialist.html",
+        "technology-capsule": "/technology-capsule.html",
+        "technology-spaceport": "/technology-spaceport.html",
+        "technology-vehicle": "/technology-vehicle.html",
+      },
+    },
+  },
 });
